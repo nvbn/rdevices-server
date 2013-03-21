39ab235382b6174869fd72ae5013858b4c53bff9
nvbn <nvbn.rm@gmail.com>
2013-03-22 00:46:56 +0400

    Add order fields to cms

--- interface/models.py
+++ interface/models.py
@@ -1,21 +1,33 @@
 from django.utils.translation import ugettext as _
-from django.contrib.auth.models import User
+from django_extensions.db.fields import AutoSlugField, CreationDateTimeField
 from django.db import models
+
+
+class EnabledManager(models.Manager):
+    """Manager with enabled check"""
+
+    def enabled(self):
+        """Get enabled entries"""
+        return self.filter(is_enabled=True)
 
 
 class NewsEntry(models.Model):
     """News entry"""
-    slug = models.SlugField(verbose_name=_('slug'))
+    slug = AutoSlugField(populate_from='title', verbose_name=_('slug'))
     is_enabled = models.BooleanField(
         default=True, verbose_name=_('is enabled'),
     )
+    created = CreationDateTimeField(verbose_name=_('created'))
     title = models.CharField(max_length=300, verbose_name=_('title'))
     preview = models.TextField(verbose_name=_('preview'))
     text = models.TextField(verbose_name=_('text'))
+
+    objects = EnabledManager()
     
     class Meta:
         verbose_name = _('News entry')
         verbose_name_plural = _('News entries')
+        ordering = ('-created',)
 
 
 class CarouselEntry(models.Model):
@@ -24,12 +36,18 @@ class CarouselEntry(models.Model):
     is_enabled = models.BooleanField(
         default=True, verbose_name=_('is enabled'),
     )
+    position = models.PositiveSmallIntegerField(
+        default=0, verbose_name=_('position'),
+    )
     title = models.CharField(max_length=300, verbose_name=_('title'))
     text = models.TextField(verbose_name=_('text'))
     image = models.ImageField(
         upload_to='carousel/%m/%d/', verbose_name=_('image'),
     )
+
+    objects = EnabledManager()
     
     class Meta:
         verbose_name = _('carousel entry')
         verbose_name_plural = _('carousel entries')
+        ordering = ('position',)
