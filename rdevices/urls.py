5916b37b99e35fe5b862700be9648e6d599b3f20
nvbn <nvbn.rm@gmail.com>
2013-03-31 21:05:52 +0400

    Fix work in small screens

--- rdevices/urls.py
+++ rdevices/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 import api_urls
 
 
@@ -18,3 +20,9 @@ urlpatterns = patterns(
     url(r'^pages', include('django.contrib.flatpages.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    ) + staticfiles_urlpatterns()
