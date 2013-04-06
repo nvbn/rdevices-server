92eb05c5a24ae5eccaf24da89a8c1330d07fe978
nvbn <nvbn.rm@gmail.com>
2013-04-07 02:42:32 +0400

    Tests for devices forms

--- devices/tests.py
+++ devices/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from devices.models import Device, DeviceMethodCall, DeviceMethod
+from devices.models import (
+    Device, DeviceMethodCall, DeviceMethod, Dashboard,
+)
 from devices.forms import (
     CreateDeviceForm, UpdateDeviceForm, DeviceMethodCallForm,
     CreateDashboardForm, UpdateDashboardForm,
@@ -32,8 +34,8 @@ class ModelTestCase(TestCase):
         call = DeviceMethodCall.objects.create(
             method=method,
             caller=self.root,
-            request='123',
-            response='456',
+            request='request',
+            response='response',
         )
 
         self.assertGreater(
@@ -73,17 +75,88 @@ class FormsTestCase(TestCase):
         )
         self.device = Device.objects.create(
             owner=self.root,
-            name='test',
-            description='test',
+            name='test name',
+            description='test description',
         )
 
     def test_create_device(self):
         """Test device creating"""
-        description = '567'
+        description = 'test description in create'
         form = CreateDeviceForm(self.root, {
-            'name': '123',
+            'name': 'test name in create',
             'description': description,
         })
         self.assertTrue(form.is_valid())
         device = form.save()
-        self.assertEqual(device.description ,description)
+        self.assertEqual(device.description, description)
+
+    def test_update_device(self):
+        """Test update device"""
+        description = 'test description in update'
+        user = User.objects.create(username='test_user')
+        form = UpdateDeviceForm({
+            'owner': user,
+            'name': 'test name in update',
+            'description': description,
+        }, instance=self.device)
+        self.assertTrue(form.is_valid())
+        device = form.save()
+        self.assertEqual(device.owner, self.root)
+        self.assertEqual(device.description, description)
+
+    def test_device_method_call(self):
+        """Test device method call form"""
+        method = DeviceMethod.objects.create(
+            name='test_method',
+            device=self.device,
+            spec={
+                'args': {
+                    'x': 'str',
+                    'y': 'str',
+                },
+                'result': 'int',
+            },
+        )
+        CallForm = DeviceMethodCallForm.create_form(method, self.root)
+
+        form = CallForm({})
+        self.assertFalse(form.is_valid())
+
+        form = CallForm({
+            'x': '14',
+            'y': '15',
+        })
+        self.assertTrue(form.is_valid())
+        call = form.save()
+        self.assertEqual(call.request['x'], '14')
+        self.assertEqual(call.request['y'], '15')
+
+    def test_create_dashboard(self):
+        """Test create dashboard form"""
+        dashboard_name = 'dashboard name in create'
+        form = CreateDashboardForm(self.root, {
+            'name': dashboard_name,
+            'description': 'dashboard description in create',
+        })
+        self.assertTrue(form.is_valid())
+        dashboard = form.save()
+        self.assertEqual(dashboard.name, dashboard_name)
+
+    def test_update_dashboard(self):
+        """Test update dashboard"""
+        dashboard = Dashboard.objects.create(
+            name='dashboard name',
+            description='dashboard description',
+            owner=self.root,
+        )
+        user = User.objects.create(username='test_user')
+        dashboard_name = 'updated dashboard name'
+        form = UpdateDashboardForm({
+            'name': dashboard_name,
+            'description': 'dashboard description',
+            'owner': user,
+        }, instance=dashboard)
+        self.assertTrue(form.is_valid())
+        dashboard = form.save()
+        self.assertEqual(dashboard.owner, self.root)
+        self.assertEqual(dashboard.name, dashboard_name)
